fa74d93e902588cd23bd1e15fdb18b39e3365833
ptums <ptumulty923@gmail.com>
2023-06-26 14:03:55 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Webcam from "react-webcam";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          width: 500,
+          height: 400,
+        }}
+      >
+        <Webcam />
+      </div>
+    </>
+  );
+}
+
+export default App;
